e2d12a633dcca69613ebbfcde9149707d9bf4c7c
itsmikesharescode <mikejohneviota@gmail.com>
2024-07-28 18:09:50 +0800

    add supabase config

---
+++ app.config.js
@@ -0,0 +1,10 @@
+export default {
+  expo: {
+    name: 'rn-proj',
+    slug: 'rn-proj',
+    extra: {
+      url: process.env.URL_KEY,
+      anon: process.env.ANON_KEY,
+    },
+  },
+};
---
+++ lib/supabase.ts
@@ -0,0 +1,15 @@
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import { createClient } from '@supabase/supabase-js';
+import Constants from 'expo-constants';
+
+const supabaseUrl = Constants.expoConfig?.extra?.url;
+const supabaseAnonKey = Constants.expoConfig?.extra?.anon;
+
+export const supabase = createClient(supabaseUrl, supabaseAnonKey, {
+  auth: {
+    storage: AsyncStorage,
+    autoRefreshToken: true,
+    persistSession: true,
+    detectSessionInUrl: false,
+  },
+});
