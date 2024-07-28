e280ce2cb70ba961b4cc0b1dca25dd887201b330
itsmikesharescode <mikejohneviota@gmail.com>
2024-07-28 12:26:41 +0800

    add tabs UI

---
+++ app/(tabs)/bookmark.tsx
@@ -0,0 +1,11 @@
+import { StyleSheet, Text, View } from 'react-native';
+
+const BookMark = () => {
+  return (
+    <View>
+      <Text>BookMark</Text>
+    </View>
+  );
+};
+
+export default BookMark;
---
+++ app/(tabs)/create.tsx
@@ -0,0 +1,11 @@
+import { StyleSheet, Text, View } from 'react-native';
+
+const Create = () => {
+  return (
+    <View>
+      <Text>Create</Text>
+    </View>
+  );
+};
+
+export default Create;
