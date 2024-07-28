33398b51d6c045f85b55e2227ff3c5a803a2f485
itsmikesharescode <mikejohneviota@gmail.com>
2024-07-28 10:37:32 +0800

    add auth root

--- app/index.tsx
+++ app/index.tsx
@@ -1,14 +1,14 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
 
 const Index = () => {
   return (
     <View>
       <Text>Index</Text>
     </View>
-  )
-}
+  );
+};
 
-export default Index
+export default Index;
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({});
