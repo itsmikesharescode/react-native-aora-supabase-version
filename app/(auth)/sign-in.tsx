900e69666518c541f33d5fb6ecd5a58b9f5b76f6
itsmikesharescode <mikejohneviota@gmail.com>
2024-07-28 10:46:19 +0800

    add custom button

---
+++ app/(auth)/sign-in.tsx
@@ -0,0 +1,12 @@
+import { Text, View, ScrollView, Image } from 'react-native';
+import { SafeAreaView } from 'react-native-safe-area-context';
+
+const SignIn = () => {
+  return (
+    <View>
+      <Text>SignIn</Text>
+    </View>
+  );
+};
+
+export default SignIn;
---
+++ components/CustomButton.tsx
@@ -0,0 +1,30 @@
+import { TouchableOpacity, Text, View } from 'react-native';
+import type { GestureResponderEvent } from 'react-native';
+
+interface CustomBotton {
+  title: string;
+  handPress: (event: GestureResponderEvent) => void;
+  containerStyle: string;
+  textStyle: string;
+  isLoading: boolean;
+}
+
+const CustomButton: React.FC<CustomBotton> = ({
+  title,
+  handPress,
+  containerStyle,
+  textStyle,
+  isLoading,
+}) => {
+  return (
+    <TouchableOpacity
+      disabled={isLoading}
+      onPress={handPress}
+      className={`bg-secondary rounded-xl min-h-[62px] justify-center items-center ${containerStyle} ${isLoading ? 'opacity-50' : ''}`}
+    >
+      <Text className={`text-primary font-psemibold text-lg ${textStyle}`}>{title}</Text>
+    </TouchableOpacity>
+  );
+};
+
+export default CustomButton;
