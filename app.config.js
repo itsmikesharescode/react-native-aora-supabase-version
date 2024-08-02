d020e8beec806628997795c38bd47be574725c9e
itsmikesharescode <mikejohneviota@gmail.com>
2024-08-02 17:30:24 +0800

    update readme

--- app.config.js
+++ app.config.js
@@ -4,6 +4,9 @@ export default ({ config }) => {
     extra: {
       url: process.env.URL_KEY,
       anon: process.env.ANON_KEY,
+      eas: {
+        projectId: 'f374e62e-d204-4ed3-bb19-a4da6117057e',
+      },
     },
   };
 };
